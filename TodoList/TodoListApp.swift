9c2ed4ce680966e4a94cd8bd28bc9baf251f6699
kxx <132kxx@gmail.com>
2023-03-02 11:03:37 +0900

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by kxx: on 2023/03/02.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
